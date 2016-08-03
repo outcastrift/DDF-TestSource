0c522284504efee0ed8b71f88e19bedb7cf6f33f
samuel.davis <sam.davis@techngs.com>
2016-08-03 13:40:02 -0400

    Fixed no cert bug

--- fedsource-test/fedsource-test-source/src/main/java/com/davis/ddf/test/fedSource/UniversalFederatedSource.java
+++ fedsource-test/fedsource-test-source/src/main/java/com/davis/ddf/test/fedSource/UniversalFederatedSource.java
@@ -828,9 +828,11 @@ public class UniversalFederatedSource implements ddf.catalog.source.FederatedSou
 
     this.ssClientCertPath = ssClientCertPath;
 
-      if(ssClientCertPassword != null) {
+    if(ssClientCertPassword != null && !ssClientCertPassword.equalsIgnoreCase("")) {
+      if (ssClientCertPath != null && !ssClientCertPath.equalsIgnoreCase("")) {
         client = new TrustingOkHttpClient().getUnsafeOkHttpClient(15, 15, getSsClientCertPath(), getSsClientCertPassword());
       }
+    }
 
   }
 
@@ -841,10 +843,11 @@ public class UniversalFederatedSource implements ddf.catalog.source.FederatedSou
   public void setSsClientCertPassword(String ssClientCertPassword) {
     LOGGER.debug("Spring setting variable ssClientCertPassword to {}", ssClientCertPassword);
     this.ssClientCertPassword = ssClientCertPassword;
-
-      if(ssClientCertPath != null) {
+    if(ssClientCertPassword != null && !ssClientCertPassword.equalsIgnoreCase("")) {
+      if (ssClientCertPath != null && !ssClientCertPath.equalsIgnoreCase("")) {
         client = new TrustingOkHttpClient().getUnsafeOkHttpClient(15, 15, getSsClientCertPath(), getSsClientCertPassword());
       }
+    }
 
   }
   public String getSsContainerPath() {
